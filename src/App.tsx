60b885655af6a465a13ab2997362fd432bfbe30d
makost <makost@ttu.ee>
2022-07-10 17:57:48 +0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import List from "./components/List";
+import data from "./data/data";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [people, setPeople] = useState(data)
+    
+    const clickHandler = () => {
+        if (people.length === 0) {
+            setPeople(data)
+        } else {
+            setPeople([])
+        }
+    }
+
+    return (
+        <main>
+            <h2>{people.length} Birthdays today</h2>
+            <section className="container">
+                <List people={people}/>
+            </section>
+            <button onClick={() => clickHandler()}>
+                    {people.length === 0 ? "Reload list" : "Clear All"}
+            </button>
+        </main>
+    );
 }
 
 export default App;
